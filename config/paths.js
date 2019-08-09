b11f3c0c5b11d7819871a7686ccd451e782fb6e8
Wojciech Reszelewski <wreszelewski@gmail.com>
2019-08-09 23:29:04 +0200

    Eslint fixes

--- config/paths.js
+++ config/paths.js
@@ -12,18 +12,18 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 const envPublicUrl = process.env.PUBLIC_URL;
 
 function ensureSlash(path, needsSlash) {
-  const hasSlash = path.endsWith('/');
-  if (hasSlash && !needsSlash) {
-    return path.substr(path, path.length - 1);
-  } else if (!hasSlash && needsSlash) {
-    return `${path}/`;
-  } else {
-    return path;
-  }
+	const hasSlash = path.endsWith('/');
+	if (hasSlash && !needsSlash) {
+		return path.substr(path, path.length - 1);
+	} else if (!hasSlash && needsSlash) {
+		return `${path}/`;
+	} else {
+		return path;
+	}
 }
 
 const getPublicUrl = appPackageJson =>
-  envPublicUrl || require(appPackageJson).homepage;
+	envPublicUrl || require(appPackageJson).homepage;
 
 // We use `PUBLIC_URL` environment variable or "homepage" field to infer
 // "public path" at which the app is served.
@@ -32,33 +32,33 @@ const getPublicUrl = appPackageJson =>
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 function getServedPath(appPackageJson) {
-  const publicUrl = getPublicUrl(appPackageJson);
-  const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
-  return ensureSlash(servedUrl, true);
+	const publicUrl = getPublicUrl(appPackageJson);
+	const servedUrl =
+		envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+	return ensureSlash(servedUrl, true);
 }
 
 // config after eject: we're in ./config/
 module.exports = {
-  dotenv: resolveApp('.env'),
-  appBuild: resolveApp('build'),
-  ssrBuild: resolveApp('functions/dist'),
-  appPublic: resolveApp('public'),
-  appHtml: resolveApp('public/index.html'),
-  appMst: resolveApp('public/indexTemplate.mst'),
-  appIndexJs: resolveApp('src/index.js'),
-  appIndexNode: resolveApp('src/index-server.js'),
-  appPackageJson: resolveApp('package.json'),
-  appSrc: resolveApp('src'),
-  yarnLockFile: resolveApp('yarn.lock'),
-  testsSetup: resolveApp('src/setupTests.js'),
-  appNodeModules: resolveApp('node_modules'),
-  publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
-  app : resolveApp('.'),
-  appConfig : resolveApp('config'),
-  cesiumDebugBuild : resolveApp('node_modules/cesium/Build/CesiumUnminified/'),
-  cesiumProdBuild : resolveApp('node_modules/cesium/Build/Cesium/'),
-  cesiumSourceFolder : resolveApp('node_modules/cesium/Source/'),
-  cesiumDll : resolveApp('distdll/cesiumDll.js')
+	dotenv: resolveApp('.env'),
+	appBuild: resolveApp('build'),
+	ssrBuild: resolveApp('functions/dist'),
+	appPublic: resolveApp('public'),
+	appHtml: resolveApp('public/index.html'),
+	appMst: resolveApp('public/indexTemplate.mst'),
+	appIndexJs: resolveApp('src/index.js'),
+	appIndexNode: resolveApp('src/index-server.js'),
+	appPackageJson: resolveApp('package.json'),
+	appSrc: resolveApp('src'),
+	yarnLockFile: resolveApp('yarn.lock'),
+	testsSetup: resolveApp('src/setupTests.js'),
+	appNodeModules: resolveApp('node_modules'),
+	publicUrl: getPublicUrl(resolveApp('package.json')),
+	servedPath: getServedPath(resolveApp('package.json')),
+	app: resolveApp('.'),
+	appConfig: resolveApp('config'),
+	cesiumDebugBuild: resolveApp('node_modules/cesium/Build/CesiumUnminified/'),
+	cesiumProdBuild: resolveApp('node_modules/cesium/Build/Cesium/'),
+	cesiumSourceFolder: resolveApp('node_modules/cesium/Source/'),
+	cesiumDll: resolveApp('distdll/cesiumDll.js')
 };
